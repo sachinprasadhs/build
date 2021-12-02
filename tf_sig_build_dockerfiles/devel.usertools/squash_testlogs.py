df6cd72512902456b93ac9bdd347485463054c3a
Nitin Srinivasan <srnitin@google.com>
2021-12-02 22:33:53 +0000

    change build enviroment to be manylinux2014 compatible
    Set up symlink for devtoolset-8
    
    Combine Docker GCR presubmits and also push main to gcr
    
    Commit missed files
    
    Log in to GCR
    
    Fix conditional, hopefully
    
    Clarify
    
    Add Python 3.10 support (#58)
    
    Adds Python 3.10 support to the containers. Python 3.10 changes some library behavior and, for now, needs an alternative installation method to work.
    
    Upgrade gcrpio for fast build and cleanup setup
    
    Add utilities for running release tests (#56)
    
    This adds the dependencies and notably bazelrc config options to run TensorFlow's Nightly and Release tests, which I've been working on replicating on internal CI. I still have documentation and migration work to do, but the major portion of the support work is here.
    
    add gdb to the system packages
    
    change to gcc 8.3.1 from centos7 for devtoolset8
    
    fix libstdc++ symlink in devtoolset-8 environment
    
    Undo ignoring other xml files
    
    Update README
    
    Deduplicate repeated messages
    
    Squash long runfiles paths
    
    Lock nvidia driver to 460
    
    libtensorflow work
    
    Fix libtensorflow script and start prelim check
    
    Update Test Requirements to have same versions as  tf_sig_build_dockerfiles/devel.requirements.txt (#65)
    
    * Add additional gitignore files
    
    * Update requirements with same versions
    
    Keep versions consistent with  tf_sig_build_dockerfiles/devel.requirements.txt
    
    Cleanup
    
    Fix Build issue from `python_include` (#67)
    
    * Remove Python 3.10 pip special handling
    
    * Link usr/include to usr/local/include
    
    * Update location of python include
    
    * Update setup.python.sh
    
    Assorted changes -- see details
    
    - Remove installation of nvidia-profiler, which depends on libcuda1,
    which ultimately installs an nvidia driver package, which we don't want
    because we're running in docker, in which the drivers are mounted. I
    hope nvidia-profiler isn't necessary for anything important; otherwise
    we'll need to synchronize driver versions between the containers and VM
    images.
    - Add less, colordiff and a newer version of clang-format
    - Add code_check_changed_files, which is intended to replace the
    "incremental" parts of ci_sanity. Still a work in progress because we
    need to decide on valuable configurations (clang-format and pylint
    cannot be run the same way as we have them configured internally and
    currently have a lot of findings)
    - Add code_check_full, which is intended to replace the "across entire
    code base" parts of ci_sanity. I rewrote many of the clunkier tests.
    Still a work in progress because we must verify that the changed tests
    will still fail.
    - Fix bad "bazel test " expansion for libtensorflow
    - Fix bad chmod for libtensoflow repacker
    
    Change libtensorflow config values to fix target selection
    
    Fix a typo in venv installation
    
    (Thanks to reedwm)
    
    Remove extra lines
    
    (Thanks again to reedwm)
    
    Clarify ctrl-s warning
    
    Correctly remove extra test filters
    
    Make it possible to run isolated pip tests
    
    More work on code checks
    
    Fix a typo
    
    Clean up code check full
    
    Remove clang-format
    
    Cleanup changed_files and move one to full
    
    Add a missing test
    
    Clean up and fix code_check_full
    
    Update docs and create experimental RBE configs
    
    Update docs and create experimental RBE configs
    
    Update dependencies to 2.9.0.dev
    
    Update Go API installation guide for TensorFlow 2.8.0 (#74)
    
    Clarify usage of nightly commit
    
    Fix mistaken 'test' command
    
    Update docs and create experimental RBE configs
    
    Update docs and create experimental RBE configs
    
    Update dependencies to 2.9.0.dev
    
    Update Go API installation guide for TensorFlow 2.8.0 (#74)
    
    Clarify usage of nightly commit
    
    Fix mistaken 'test' command
    
    change to devtoolset-9 and gcc 9.3.1 for manylinux2014

---
+++ tf_sig_build_dockerfiles/devel.usertools/squash_testlogs.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+#
+# Usage: squash_testlogs.py START_DIRECTORY OUTPUT_FILE
+#
+# Example: squash_testlogs.py /tf/pkg/testlogs /tf/pkg/merged.xml
+#
+# Recursively find all the JUnit test.xml files in one directory, and merge any
+# of them that contain failures into one file. The TensorFlow DevInfra team
+# uses this to generate a simple overview of an entire pip and nonpip test
+# invocation, since the normal logs that Bazel creates are too large for the
+# internal invocation viewer.
+import glob
+import os
+import sys
+from junitparser import JUnitXml
+from lxml import etree
+import subprocess
+import re
+
+result = JUnitXml()
+try:
+  files = subprocess.check_output(["grep", "-rlE", '(failures|errors)="[1-9]', sys.argv[1]])
+except subprocess.CalledProcessError as e:
+  print("No failures found to log!")
+  exit(0)
+
+# For test cases, only show the ones that failed that have text (a log)
+seen = set()
+runfiles_matcher = re.compile(r"(/.*\.runfiles/)")
+
+for f in files.strip().splitlines():
+  # Just ignore any failures, they're probably not important
+  try:
+    r = JUnitXml.fromfile(f)
+  except Exception as e: 
+    print("Ignoring this XML parse failure in {}: ".format(f), str(e))
+
+  for testsuite in r:
+    # Remove empty testcases
+    for p in testsuite._elem.xpath('.//testcase'):
+      if not len(p):
+        testsuite._elem.remove(p)
+    # Convert "testsuite > testcase,system-out" to "testsuite > testcase > error"
+    for p in testsuite._elem.xpath('.//system-out'):
+      for c in p.getparent().xpath('.//error | .//failure'):
+        c.text = p.text
+      p.getparent().remove(p)
+    # Remove duplicate results of the same exact test (e.g. due to retry attempts)
+    for p in testsuite._elem.xpath('.//error | .//failure'):
+      key = p.getparent().get("name", "") + p.text
+      if key in seen:
+        testsuite._elem.remove(p.getparent())
+      else:
+        seen.add(key)
+    # Include helpful notes
+    for p in testsuite._elem.xpath('.//error | .//failure'):
+      short_name = re.search(r'/(bazel_pip|tensorflow)/.*', f.decode("utf-8")).group(0)
+      p.text += f"\nNOTE: From /{short_name}"
+      p.text = runfiles_matcher.sub("[testroot]/", p.text)
+      if "bazel_pip" in short_name:
+        p.text += "\nNOTE: This is a --config=pip test. Remove 'bazel_pip' to find the file."
+      p.text += f"\nNOTE: The list of failures from the XML includes flakes and attempts as well."
+      p.text += f"\n      The error(s) that caused the invocation to fail may not include this testcase."
+    # Remove this testsuite if it doesn't have anything in it any more
+    if len(testsuite) == 0:
+      r._elem.remove(testsuite._elem)
+  if len(r) > 0:
+    result += r
+
+os.makedirs(os.path.dirname(sys.argv[2]), exist_ok=True)
+result.update_statistics()
+result.write(sys.argv[2])
